984e71f31c1cb47fcd820a997bcf0297182896ec
khalil NOUI <noui.khalil.pr@gmail.com>
2024-10-06 15:57:07 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching (drivers, theDriver) {
+    const match = drivers.filter((ele) => ele.toLowerCase() === theDriver.toLowerCase(), theDriver);
+    return match;
+}
+
+
+function fuzzyMatch (drivers, theLetter) {
+    const match = drivers.filter((ele) => ele.substr(0,theLetter.length) === theLetter, theLetter);
+    return match;
+}
+
+function matchName (drivers, theArg) {
+    const match = drivers.filter((ele) => ele.name === theArg )
+    return match
+}
